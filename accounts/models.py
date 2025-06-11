15a19645605771f54304c894209fca74d0c0f883
= <=>
2025-06-11 14:36:01 +0330

    fix refral issue

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,7 @@ class Users(models.Model):
     username = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=300)
-    referral_code = models.CharField(max_length=100, blank=True, null=True)
+    referral_code = models.CharField(max_length=100, blank=True, null=True)  # Field for referral code
+    referred_by = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, blank=True, null=True, related_name='referrals'
+    )  # Tracks who referred the user
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,22 +1,32 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from .models import Users  # Use your custom Users model
 
 class UserSignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)  # Password should not be returned in responses
     referral_code = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
-        model = User
+        model = Users
         fields = ['username', 'email', 'password', 'referral_code']
 
     def create(self, validated_data):
-        # This method is called when serializer.save() is called
-        # Create user with hashed password (using create_user helper)
+        # Extract referral code if provided
         referral_code = validated_data.pop('referral_code', None)
-        user = User.objects.create_user(
+
+        # Create the user
+        user = Users.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        # Optionally handle referral_code logic here
+
+        # If a referral code is provided, link the user to the referrer
+        if referral_code:
+            try:
+                referrer = Users.objects.get(referral_code=referral_code)
+                user.referred_by = referrer
+                user.save()
+            except Users.DoesNotExist:
+                pass  # Ignore if the referral code is invalid
+
         return user
