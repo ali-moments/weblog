89ccc682b183e825577bce2c64a5cf202e468795
= <=>
2025-06-11 14:18:40 +0330

    add refral code

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ class Users(models.Model):
     username = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=300)
-    
+    referral_code = models.CharField(max_length=100, blank=True, null=True)
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,17 +3,20 @@ from django.contrib.auth.models import User
 
 class UserSignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)  # Password should not be returned in responses
+    referral_code = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'referral_code']
 
     def create(self, validated_data):
         # This method is called when serializer.save() is called
         # Create user with hashed password (using create_user helper)
+        referral_code = validated_data.pop('referral_code', None)
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+        # Optionally handle referral_code logic here
         return user
