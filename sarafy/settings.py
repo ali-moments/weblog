9c03e9f20a679de4017ef4747b1f846a0693fcba
Momento <a.ham2334@gmail.com>
2025-06-11 13:23:43 +0330

    Update settings.py

--- sarafy/settings.py
+++ sarafy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from ../constants.py import DJANGO_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pf0yw%o69w*4vr&3db+c8!-y=uh5)1s$#5rhek(*+yk)%g)sw='
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
